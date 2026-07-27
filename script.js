f2f5ef658c6948d2f2a9cdd5b3dc169309acca6c
Kabom-mm3 <m.razzanalim@semesta.sch.id>
2026-07-27 15:03:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,14 @@
 // ==========================
 
 // ====== LIST FOTO ======
-const photos = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg", 
-    "10.jpg",
-    "11.jpg",
-    "12.jpg",
+const album = [
+
+    {type:"image",file:"1.jpg"},
+    {type:"image",file:"2.jpg"},
+    {type:"video",file:"1.mp4"},
+    {type:"image",file:"3.jpg"},
+    {type:"video",file:"2.mp4"}
+
 ];
 
 // ====== ELEMENT ======
